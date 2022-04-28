17f8898750856be6b7e6fd5deed37f974b2f9442
reuhreuh <2742045+reuhreuh@users.noreply.github.com>
2022-04-28 10:28:36 +0200

    Create pools.js

---
+++ pools.js
@@ -0,0 +1,19 @@
+const POOLS_TRAITS = new Map([
+    ['VEGAS',[{'Background': 'Baby blue'},{'Background': 'Vegas'},{'Background': 'Lunar new year blue'},{"Fur": 'Radioactive Pink'},{"Hat": 'Flat peak'},{"Mouth": 'Meh'},{"Mouth Decoration": 'Cigarette'},{"Glasses": 'Green laser'},{"Frame": 'Fairy lights'}]],
+    ['ALLI',[{'Background': 'Red'},{'Background': 'Fireworks'},{"Fur": 'White'},{"Ear Decoration": null},{"Hat": 'Top hat red'},{"Mouth": 'Sad'},{"Mouth Decoration": 'Sick'},{"Glasses": 'Visor heart'},{"Frame": 'Fish bowl'}]],
+    ['FARM',[{'Background': 'Khaki'},{'Background': 'Fireplace'},{"Fur": 'Gold'},{"Ear Decoration": null},{"Hat": 'Robin Hood'},{"Mouth Decoration": 'Flaming Hot'},{"Neck":'Blue Collar Gold Spikes'},{"Glasses": 'Visor Beam'},{"Frame": 'Poo'}]],
+    ['SEA',[{'Background': 'Navy'},{"Fur": 'Holographic'},{"Hat": 'Reindeer'},{"Hat": 'Silver Crown'},{"Mouth Decoration": 'Snorkle'},{"Glasses": 'Eyepatch Right'},{"Glasses": 'Eyepatch Left'},{"Glasses": 'Red Laser'},{"Frame": 'Test Card'}]],
+    ['ABC',[{'Background': null},{"Fur": null},{"Eyes": null},{"Ear Decoration": null},{"Hat": null},{"Mouth": null},{"Mouth Decoration": null},{"Neck":null},{"Glasses": null},{"Frame": null}]],
+    ['JACK',[{'Background': null},{"Fur": null},{"Eyes": null},{"Ear Decoration": null},{"Hat": null},{"Mouth": null},{"Mouth Decoration": null},{"Neck":null},{"Glasses": null},{"Frame": null}]],
+    ['PRIDE',[{'Background': null},{"Fur": null},{"Eyes": null},{"Ear Decoration": null},{"Hat": null},{"Mouth": null},{"Mouth Decoration": null},{"Neck":null},{"Glasses": null},{"Frame": null}]],
+    ['FIKA',[{'Background': null},{"Fur": null},{"Eyes": null},{"Ear Decoration": null},{"Hat": null},{"Mouth": null},{"Mouth Decoration": null},{"Neck":null},{"Glasses": null},{"Frame": null}]],
+    ['HERO',[{'Background': null},{"Fur": null},{"Eyes": null},{"Ear Decoration": null},{"Hat": null},{"Mouth": null},{"Mouth Decoration": null},{"Neck":null},{"Glasses": null},{"Frame": null}]],
+    ['PSYA',[{'Background': null},{"Fur": null},{"Eyes": null},{"Ear Decoration": null},{"Hat": null},{"Mouth": null},{"Mouth Decoration": null},{"Neck":null},{"Glasses": null},{"Frame": null}]],
+    ['WOOF',[{'Background': null},{"Fur": null},{"Eyes": null},{"Ear Decoration": null},{"Hat": null},{"Mouth": null},{"Mouth Decoration": null},{"Neck":null},{"Glasses": null},{"Frame": null}]],
+    ['A3C',[{'Background': 'Late Night'},{"Fur": "Pink"},{"Eyes": null},{"Ear Decoration": null},{"Hat": "Crown"},{"Mouth": null},{"Mouth Decoration": "Big Moustache"},{"Neck":"Blue Collar"},{"Glasses": "Visor Dead"},{"Frame": "Damien Hirst"}]],
+    ['CHEF',[{'Background': null},{"Fur": null},{"Eyes": null},{"Ear Decoration": null},{"Hat": null},{"Mouth": null},{"Mouth Decoration": null},{"Neck":null},{"Glasses": null},{"Frame": null}]],
+    ['ITZA',[{'Background': null},{"Fur": null},{"Eyes": null},{"Ear Decoration": null},{"Hat": null},{"Mouth": null},{"Mouth Decoration": null},{"Neck":null},{"Glasses": null},{"Frame": null}]],
+    ['PSB',[{'Background': 'Prison'},{"Fur": 'Hoskasaur'},{"Eyes": null},{"Ear Decoration": 'Stud Left'},{"Hat": 'Red Lunar New Year Hat'},{"Mouth": 'Happy'},{"Glasses": '3D Gloss'},{"Frame": 'Lunar New Year Wave'}]],
+    ['QCPOL',[{'Background': null},{"Fur": null},{"Eyes": null},{"Ear Decoration": null},{"Hat": null},{"Mouth": null},{"Mouth Decoration": null},{"Neck":null},{"Glasses": null},{"Frame": null}]],
+    ['SALT',[{'Background': null},{"Fur": null},{"Eyes": null},{"Ear Decoration": null},{"Hat": null},{"Mouth": null},{"Mouth Decoration": null},{"Neck":null},{"Glasses": null},{"Frame": null}]]
+]);
